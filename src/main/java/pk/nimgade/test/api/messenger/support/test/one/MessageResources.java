ebbf8bc8494218f1d31bcbfc21e17a5c0e9a56b3
pankajnimgade <pankaj.nimgade@gmail.com>
2016-03-22 00:08:27 +0530

    implemented POST method to create new Message

--- src/main/java/pk/nimgade/test/api/messenger/support/test/one/MessageResources.java
+++ src/main/java/pk/nimgade/test/api/messenger/support/test/one/MessageResources.java
@@ -2,7 +2,9 @@ package pk.nimgade.test.api.messenger.support.test.one;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -35,6 +37,13 @@ public class MessageResources {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageID") long messageID){
 		return messageService.getMessage(messageID);
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message postMessage( Message message){
+		return messageService.addMessage(message);
 	}
 
 }
